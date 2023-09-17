9bf4309994c42df56d8d49a708bf196b13308e4e
Marco Ballarini <56921877+MrcBalla@users.noreply.github.com>
2023-09-17 23:04:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from simulationFunction import *
 
+
+# qui si tratta solamente una prova, si potranno scegliere 
+# altre funzioni da testare all'interno del file simulationFunction.
 def main():
     calcolo_numero_item_medio()
 
